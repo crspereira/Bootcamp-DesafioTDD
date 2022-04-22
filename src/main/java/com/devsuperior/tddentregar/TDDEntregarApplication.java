1cfbcaed8b9fbc818b8615e18d2c570c93d3642a
Clayton Pereira <clayton.pereira@outlook.com>
2022-04-22 17:15:42 -0300

    BackEnd Cap-2 - Created Project TDD Challenge

---
+++ src/main/java/com/devsuperior/tddentregar/TDDEntregarApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.tddentregar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TDDEntregarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TDDEntregarApplication.class, args);
+	}
+
+}
