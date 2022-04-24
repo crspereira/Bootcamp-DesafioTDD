0fae91e759fac90b83dd59786aa2b881ac0f8630
Clayton Pereira <clayton.pereira@outlook.com>
2022-04-24 02:35:38 -0300

    BackEnd Cap-2 - Created CityController delete - Test OK

--- src/main/java/com/devsuperior/tddentregar/controllers/CityController.java
+++ src/main/java/com/devsuperior/tddentregar/controllers/CityController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,12 @@ public class CityController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
 				.buildAndExpand(cityDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(cityDto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		cityService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/devsuperior/tddentregar/services/CityService.java
+++ src/main/java/com/devsuperior/tddentregar/services/CityService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.tddentregar.dto.CityDTO;
 import com.devsuperior.tddentregar.entities.City;
 import com.devsuperior.tddentregar.repositories.CityRepository;
+import com.devsuperior.tddentregar.services.exceptions.ServicesDataBaseException;
+import com.devsuperior.tddentregar.services.exceptions.ServicesNotFoundException;
 
 @Service
 public class CityService {
@@ -30,6 +34,16 @@ public class CityService {
 		city.setName(cityDto.getName());
 		cityRepository.save(city);
 		return new CityDTO(city);
+	}
+	
+	public void delete(Long id) {
+		try {
+			cityRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ServicesNotFoundException("ID ["+ id +"] Not Found!");
+		} catch (DataIntegrityViolationException e) {
+			throw new ServicesDataBaseException("DataBase Integrity Violation!");
+		}
 	}
 
 }
